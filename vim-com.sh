01f1607fb439a661b7cad3029bfbeaeb9d4571c3
foxleoly <foxleoly@live.com>
2022-02-06 14:46:52 +0800

    update vim compile script

--- vim-com.sh
+++ vim-com.sh
@@ -4,18 +4,25 @@ cd /home/foxleoly/workspace/vim
 git pull
 cd src
 echo "clean VIM folder...."
+sudo make clean
 sudo make distclean
 echo "Compile VIM  ...."
-uname="$(uname -s)"
+# uname="$(uname -s)"
 # check the uname output
-if [ uname == "Linux" ]
+cmds=""
+if [ $(uname -s) == "Linux" ]
 then 
-	echo "OS:==> $uname"
-	./configure --prefix=/home/foxleoly/workspace/tools/vim --enable-gui=no --without-x --enable-cscope --enable-multibyte --enable-python3interp=yes  --with-python3-command=python3 --enable-pythoninterp=yes  --with-python-command=python2
+echo "OS:==> $(uname -s)"
+cmds="./configure --prefix=/home/foxleoly/workspace/tools/vim --enable-gui=no --without-x --enable-cscope --enable-multibyte --enable-python3interp=yes  --with-python3-command=python3 --enable-pythoninterp=yes  --with-python-command=python2"
 else
 # configure command setup for mac
-	echo "OS:==> $(uname -s)"
+     echo "OS:==> $(uname -s)"
+     cmds="./configure --prefix=/home/foxleoly/workspace/tools/vim --enable-gui=no --without-x --enable-cscope --enable-multibyte --enable-python3interp=yes  --with-python3-command=python3 --enable-pythoninterp=yes  --with-python-command=python2"
 fi
+echo "execute below command :=======>\n $cmds"
+sleep 3s
+output="$(exec $cmds)"
+echo "$output"
 sudo make 
 echo "Sleep 3s for install ...."
 sleep 3s
